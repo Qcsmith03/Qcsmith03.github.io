de6996cbea669e14bd4a39dbe8d49803a542b663
Qcsmith03 <91340462+Qcsmith03@users.noreply.github.com>
2021-09-24 12:01:23 -0600

    added template

---
+++ Template/sketch.js
@@ -0,0 +1,16 @@
+// first demo
+// Quinten Smith
+// 9/24/2021
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  rect(mouseX,mouseY,100,50);
+}
