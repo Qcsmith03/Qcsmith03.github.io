585c733e2392970face6a790d5d6917da8cf0c4b
Qcsmith03 <91340462+Qcsmith03@users.noreply.github.com>
2021-10-04 12:39:53 -0600

    capatilized names

--- interactive scene pong/sketch.js
+++ interactive scene pong/sketch.js
@@ -1,16 +1,16 @@
 // extra for expert is the scroll wheel. it will increase ball size.
 // dx and dy are the x and y speed
-let leftpadalx = 15;
-let leftpadaly = 150;
-let rightpadalx = 1550;
-let rightpadaly = 150;
+let leftPadalX = 15;
+let leftPadalY = 150;
+let rightPadalX = 1550;
+let rightPadalY = 150;
 let radius = 10;
 let dx = 5;
 let dy = 5;
 let ballx = 200;
 let bally = 100;
 let hit = false;
-let texttime = 0;
+let textTime = 0;
 
 function setup() {
   createCanvas(windowWidth-30, windowHeight-30);
@@ -19,14 +19,14 @@ function setup() {
 //draw setup
 function draw() {
   background(0);
-  shapedraw();
-  movepong();
-  moveandbounce();
-  speedincrease();
+  shapeDraw();
+  movePong();
+  moveAndBounce();
+  speedIncrease();
   pause();
   
   //rules
-  if (millis() < texttime + 5000){
+  if (millis() < textTime + 5000){
     fill(255);
     textSize(25);
     text("play on full screen,r is pause, lmb moves ball right, scroll wheel is to increase ball size", 10, 50);
@@ -34,13 +34,13 @@ function draw() {
   console.log(millis);
 }
 //draws padals and ball
-function shapedraw() {
-  rect(leftpadalx, leftpadaly, 10, 70);
-  rect(rightpadalx, rightpadaly, 10, 70);
+function shapeDraw() {
+  rect(leftPadalX, leftPadalY, 10, 70);
+  rect(rightPadalX, rightPadalY, 10, 70);
   circle(ballx, bally, radius);
 }
 //moves ball and brings back to start point, as well as the collision of padal and ball.
-function moveandbounce() {
+function moveAndBounce() {
   ballx += dx;
   bally += dy;
 
@@ -55,11 +55,11 @@ function moveandbounce() {
     
     
   }
-  hit = collideRectCircle(rightpadalx, rightpadaly, 10, 70, ballx, bally, radius);
+  hit = collideRectCircle(rightPadalX, rightPadalY, 10, 70, ballx, bally, radius);
   if (hit === true){
     dx =-dx ;
   }
-  hit = collideRectCircle(leftpadalx, leftpadaly, 10, 70, ballx, bally, radius);
+  hit = collideRectCircle(leftPadalX, leftPadalY, 10, 70, ballx, bally, radius);
   if (hit === true){
     dx =-dx ;
   }
@@ -67,26 +67,27 @@ function moveandbounce() {
     
 }
 //moves the right and left padal.
-function movepong() {
+function movePong() {
   if (keyIsDown(87)) {
     // w is up
-    leftpadaly -= 7;
+    leftPadalY -= 7;
   }
 
   if (keyIsDown(83)) {
     // s is down
-    leftpadaly += 7;
+    leftPadalY += 7;
   }
   if (keyIsDown(UP_ARROW)) {
     // up arrow is up
-    rightpadaly -= 7;
+    rightPadalY -= 7;
   }
 
   if (keyIsDown(DOWN_ARROW)) {
     // down arrow is down
-    rightpadaly += 7;
+    rightPadalY += 7;
   }
 }
+
 //brings ball to original postion and says player one wins
 function game() {
   console.log("p1 win");
@@ -100,6 +101,7 @@ function game() {
   text("p1 wins", windowWidth/2, 75);
   
 }
+
 // brings ball to original and says player 2 wins
 function game2(){
   console.log("p2 win");
@@ -118,17 +120,20 @@ function mousePressed(){
   dx = dx+1;
   
 }
+
 // increases speed on colliision
-function speedincrease(){
+function speedIncrease(){
   if (hit===true){
     dx = dx +1;
   }
 }
+
 // increases the size of the ball
 function mouseWheel(event){
   radius += 5;
   return false;
 }
+
 // press r to stop the x movment of the ball
 function pause(){
   if (keyIsDown(82)){
