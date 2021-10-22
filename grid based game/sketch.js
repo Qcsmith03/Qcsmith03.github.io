a4ee4a4c5822f4a924d3c5b6446c9e68cb6eb79d
Qcsmith03 <91340462+Qcsmith03@users.noreply.github.com>
2021-10-22 12:14:01 -0600

    starting simon says

--- grid based game/sketch.js
+++ grid based game/sketch.js
@@ -2,33 +2,38 @@
 // Quinten Smith
 
 let grid;
-let gridSize = 20;
+let gridSize = 2;
 let cellWidth, cellHeight;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cellWidth = width/gridSize;
   cellHeight = height/gridSize;
-  grid = createEmpty2DArray(gridSize,gridSize);
+  grid = createRandom2DArray(gridSize,gridSize);
 
 }
 
 function draw() {
   background(220);
-  displayGrid;
+  displayGrid();
 }
 function displayGrid() {
   for (let y=0; y<gridSize; y++) {
     for (let x=0; x<gridSize; x++) {
       if (grid[y][x] === 0) {
-        fill("white");
+        fill("green");
+        //fill(random(255),random(255),random(255));
       }
       if (grid[y][x] === 1) {
-        fill("black");
+        fill("blue");
       }
       if (grid[y][x] === 9) {
         fill("red");
       }
+      if (grid[y][x]===2){
+        fill("yellow");
+      }
+      strokeWeight(3);
       rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
     }
   }
@@ -43,4 +48,48 @@ function createEmpty2DArray(rows, cols) {
     }
   }
   return board;
+}
+function mousePressed(){
+  let cellWidth= width/gridSize;
+  let cellHeight = height/gridSize;
+  
+  let cellx = Math.floor(mouseX/cellWidth);
+  let celly= Math.floor(mouseY/cellHeight);
+
+  if(grid[celly][cellx]===1){
+    grid[celly][cellx]=9;  
+  }
+  else if (grid[celly][cellx]===0){
+    grid[celly][cellx]=1;
+  }
+  else if (grid[celly][cellx]===9){
+    grid[celly][cellx]=2;
+  }
+  else if (grid[celly][cellx]===2){
+    grid[celly][cellx]=0;
+  }
+
+}
+function createRandom2DArray(rows,cols){
+  let grid=[];
+  for (let y=0;y<rows;y++){
+    grid.push([]);
+    for(let x=0;x<cols;x++){
+      if (random(100)<50){
+        grid[y].push(0);
+      }
+      else {
+        grid[y].push(1);
+      }
+    }
+  }
+  return grid;
+}
+function keyPressed(){
+  if (keyCode===82){
+    setup();
+  }
+  if (keyCode===69){
+    grid =createEmpty2DArray(gridSize,gridSize);
+  }
 }
