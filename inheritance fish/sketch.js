c3be029ea98a9c9e742b9218e612aeecf3940504
Qcsmith03 <91340462+Qcsmith03@users.noreply.github.com>
2021-12-16 11:58:05 -0600

    made a cool thing

---
+++ inheritance fish/sketch.js
@@ -0,0 +1,95 @@
+// Aquarium
+
+
+let theCreature=[];
+let clownfishImg;
+let octopusImg;
+
+function preload() {
+  clownfishImg = loadImage("assets/clownfish.png");
+  octopusImg = loadImage("assets/octopus.png");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  for (let i =0; i<50;i++){
+    if (random(100) < 30){
+      let octopus = new Octopus(100, 200, 100, octopusImg);
+      theCreature.push(octopus);
+    }
+    else { 
+      let fish = new Clownfish(random(width), random(100, height-100), 30, clownfishImg);
+      theCreature.push(fish);
+    }
+  }
+  
+}
+
+function draw() {
+  background(220);
+  for (let someCreature of theCreature){
+    someCreature.update();
+    someCreature.display();
+  }
+}
+
+class Creature {
+  constructor(x, y, size) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+  }
+
+  update() {
+    //Declan's Movement
+    this.x += 2;
+    this.y += sin(this.x / 100);
+  }
+
+  display() {
+    fill("green");
+    circle(this.x, this.y, this.size);
+  }
+}
+
+class Clownfish extends Creature {
+  constructor(x, y, size, theImage) {
+    super(x, y, size);
+    this.myImage = theImage;
+    this.yTime = 1000;
+    this.speed = random(1, 2);
+  }
+
+  update() {
+    //Logan's movement
+    // this.x += 2;
+    // this.y = noise(this.yTime)*height;
+    // this.yTime += random(0.001, 0.005);
+
+    
+
+    //Daniel's Movement
+    this.x += random(0.1, 3);
+    this.y = noise(this.x/300, second()/600) * height;
+
+    // //teleport from right to left
+    if (this.x > width) {
+      this.x = 0;
+    }
+  }
+
+  display() {
+    image(this.myImage, this.x, this.y, this.size, this.size);
+  }
+}
+
+class Octopus extends Creature {
+  constructor(x, y, size, someImage) {
+    super(x, y, size);
+    this.image = someImage;
+  }
+
+  display() {
+    image(this.image, this.x, this.y, this.size, this.size);
+  }
+}
